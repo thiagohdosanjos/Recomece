d71576e57a4103c322ce34f2002b55da178c5f71
Jéssica Santos <73312789+JessicaSantoss@users.noreply.github.com>
2021-01-27 22:09:50 -0300

    Implementação Model

---
+++ redeRecomece/src/app/model/Tema.ts
@@ -0,0 +1,9 @@
+import { Postagem } from "./Postagem";
+
+export class Tema{
+    public idTema: number;
+    public categoria: string;
+    public nome: string;
+    public postagem: Postagem[]
+
+}
---
+++ redeRecomece/src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+    public id: number
+    public nome: string
+    public userName: string
+    public senha: string
+    public token: string
+    public foto: string
+    public tipo: string
+}
